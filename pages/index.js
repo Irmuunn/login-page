610d867da59af032c3038f0e01321a7d89354f8b
Tudu <tuuduu@Tudus-MacBook-Pro.local>
2022-10-19 10:49:12 +0800

    second

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,12 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="flex">
+        <div></div>
+        <div className="bg-cyan-100"></div>
+      </div>
+    </>
+  );
 }
