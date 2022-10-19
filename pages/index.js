42432dc99c79d6d828d3836b34e9e460a9b57c48
Tudu <tuuduu@Tudus-MacBook-Pro.local>
2022-10-19 11:18:55 +0800

    third

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,12 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <>
-      <div className="flex">
-        <div></div>
-        <div className="bg-cyan-100 justify-center align-center"></div>
+      <div className="flex w-screen h-screen">
+        <div className="w-1/2 h-full"></div>
+        <div className="bg-cyan-50 flex justify-center  items-center h-full w-1/2 relative">
+          <div className="rounded-full bg-indigo-700 w-80 h-80"></div>
+          <div className="absolute bottom-0 h-1/2 w-full bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg"></div>
+        </div>
       </div>
     </>
   );
